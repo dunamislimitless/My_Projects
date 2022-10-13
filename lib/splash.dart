b00ccbe9bc731ea56bbbdbb11ed2f083f4eb88e2
dunamislimitless <ogundojusamuel@gmail.com>
2022-10-13 22:44:41 +0100

    pushing from comfort.

--- lib/splash.dart
+++ lib/splash.dart
@@ -6,47 +6,55 @@ import 'package:my_project/onboarding.dart';
 import 'package:my_project/pageone.dart';
 
 class Splash extends StatefulWidget {
-  const Splash({ Key? key }) : super(key: key);
+  const Splash({Key? key}) : super(key: key);
 
   @override
   State<Splash> createState() => _SplashState();
 }
 
 class _SplashState extends State<Splash> {
-  void initState(){
+  void initState() {
     super.initState();
-    Timer(Duration(seconds: 4), (){
-       Navigator.pushReplacement(context, MaterialPageRoute(builder: (BuildContext context) => Onboarding()));
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushReplacement(context,
+          MaterialPageRoute(builder: (BuildContext context) => Onboarding()));
     });
-  }  
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor:HexColor('#1A936F'),
-      body:SafeArea(
+      backgroundColor: HexColor('#1A936F'),
+      body: SafeArea(
         child: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
-                Container(
-                          height: 119,
-                          width: 198,
-                          child: Image(image: AssetImage('assets/images/logintop.png'),),),
-                          SizedBox(
-                            height: 20,
-                          ),
-                Text('VFX LOGISTICS',style:TextStyle(fontSize: 36,fontWeight: FontWeight.bold,color: Colors.white)),
-                SizedBox(
-                            height: 10,
-                          ),
-                Text('....we move anything, seriously.',style:TextStyle(fontSize: 14,color: Colors.white)),
-      
+              Container(
+                height: 119,
+                width: 198,
+                child: Image(
+                  image: AssetImage('assets/images/logintop.png'),
+                ),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text('VFX LOGISTICS',
+                  style: TextStyle(
+                      fontSize: 36,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white)),
+              SizedBox(
+                height: 10,
+              ),
+              Text('....we move anything, seriously.',
+                  style: TextStyle(fontSize: 14, color: Colors.white)),
             ],
           ),
         ),
       ),
-      
     );
   }
 }
